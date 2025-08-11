645080cd75bab4aa7532b5c454f3e6862fbbebe0
DEVMarlosGomes <corretormarlosgomes@gmail.com>
2025-08-11 15:12:22 -0300

    Store

---
+++ marlos-shop/src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HeroSection.css';
+
+function HeroSection({ onLoginClick }) {
+  return (
+    <div className="hero">
+      <h1>Bem-vindo à Marlos Store
+      </h1>
+      <p>Os melhores produtos você encontra aqui!</p>
+      <button className="btn-login" onClick={onLoginClick}>Login</button>
+    </div>
+  );
+}
+
+export default HeroSection;
